ae57934eae5d6fdfb397b27c5fdda0519e199099
Lukas Petersen <lukas.petersen93@yahoo.de>
2022-01-24 00:48:17 +0100

    Update DatabaseApp.py
    Add ability to pass product index

--- DatabaseApp.py
+++ DatabaseApp.py
@@ -1,11 +1,18 @@
+from turtle import onclick
 import streamlit as st
 import pandas as pd
 import json
 from DatabaseAppHelper import *
 
+if 'product_index' not in st.session_state:
+    st.session_state.product_index = None
+    
+def handle_buttonpress(x):
+    st.session_state.product_index = x
+
 with st.sidebar:
-    st.header("Databank Selection")
-    uploaded_file = st.file_uploader("Choose a databank",type="json")
+    st.header("Datenbank-Auswahl")
+    uploaded_file = st.file_uploader("Datenbank auswählen",type="json")
     product_search = st.text_input('Produkt suchen')
     
 if uploaded_file is None or product_search is None:
@@ -17,9 +24,9 @@ query_result = data.get(product_search)
 if query_result is None:
     st.write("Keine Daten verfügbar")
     st.stop()
-    
-detail_data = query_result[0]["Details"]
-detail_df = pd.DataFrame(detail_data,index=pd.RangeIndex(0,len(detail_data)))
+
+detail_data = [pd.DataFrame(query_result[i].get("Details"),index=[i]) for i in range(len(query_result))]
+detail_df = pd.concat(detail_data)
 
 # # Show user table 
 colms = st.columns((1, 2, 1, 1, 1, 1))
@@ -36,6 +43,11 @@ for x, email in enumerate(detail_df['Produktname']):
     col4.write(detail_df['Temperatur'][x])   # Temperatur
     col5.write(detail_df['Datum'][x]) # Datum
     button_phold = col6.empty()  # create a placeholder
-    do_action = button_phold.button("Choose", key=x)
+    do_action = button_phold.button("Details",on_click=handle_buttonpress, args = (x,), key=x)
     if do_action:
-            pass # do some action with a row's data
+        pass
+
+
+if st.session_state.product_index is None:
+    st.stop()
+st.write(st.session_state.product_index)
